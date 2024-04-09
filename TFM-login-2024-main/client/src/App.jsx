304ce589a4ea969181f01d25743fd78738fa4fd1
daw2-ramirez22 <ramirezsanchezcristian@fpllefia.com>
2024-04-09 21:25:29 +0200

    Registros de usuario desde el login

--- TFM-login-2024-main/client/src/App.jsx
+++ TFM-login-2024-main/client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<h1>Home Page</h1>}></Route>
-                <Route path="/login" element={<h1>Login</h1>}></Route>
-                <Route path="/register" element={<h1>Register</h1>}></Route>
+                <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+                <Route path="/register" element={<RegisterPage></RegisterPage>}></Route>
                 <Route path="/tasks" element={<h1>Task Page</h1>}></Route>
                 <Route path="/add-task" element={<h1>New Task</h1>}></Route>
                 <Route path="/task/:id" element={<h1>Update Task</h1>}></Route>
