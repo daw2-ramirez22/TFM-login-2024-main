532c6d3536d0bbbc44c0adfb0b7d84f519903613
daw2-ramirez22 <ramirezsanchezcristian@fpllefia.com>
2024-04-24 20:19:58 +0200

    añado api a rutas del back

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,14 +8,17 @@ import {registerSchema, loginSchema} from '../schemas/auth.schema.js'
 const router = Router()
 //rutas post
 //traigo desde middleware la validacion de registro
-router.post ('/register', validateSchema(registerSchema), register)
+router.post ('/api/register', validateSchema(registerSchema), register)
 //traigo desde middleware la validacion de login
-router.post ('/login', validateSchema(loginSchema), login)
-router.post ('/logout', logout)
+router.post ('/api/login', validateSchema(loginSchema), login)
+router.post ('/api/logout', logout)
 //rutas get
-router.get ('/verify', verifyToken)
+router.get ('/api/verify', verifyToken)
 
-router.get ('/profile', authRequired , profile)
+router.get ('/api/profile', authRequired , profile)
+
+
+
 
 // para exportar los enrutadores de login y register
 export default router
