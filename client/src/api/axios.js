4728b18f85e531d5726d7dccb33f36ffe43f885f
daw2-ramirez22 <ramirezsanchezcristian@fpllefia.com>
2024-04-24 18:03:26 +0200

    cambio ruta api en cors y front

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //creo la instancia base de todas las promesas luego la importare en axios en task y auth directamente desde aqui
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://tfm-login-2024-main.onrender.com",
     withCredentials: true
 })
 export default instance
